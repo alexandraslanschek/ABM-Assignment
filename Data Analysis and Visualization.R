c81946e8326459fb2b8bc5147dfd21eb698640d9
dorianboehler <71817806+dorianboehler@users.noreply.github.com>
2024-12-03 17:07:01 +0100

    Improve data analysis and visualisation

--- Data Analysis and Visualization.R
+++ Data Analysis and Visualization.R
@@ -20,7 +20,7 @@ data <- mutate(data, across(c(max_sick_leave_days, number_of_workers, number_of_
 # Check values of independent variables
 for(i in c('max_sick_leave_days', 'number_of_workers', 'number_of_teams', 'movement_across_teams', 'share_of_presentees')) {
   print(unique(data[, i]))
-}
+} # Good
 
 remove(i)
 
@@ -48,11 +48,11 @@ ggplot(data = filter(plot, day <= 65 & group == 'Absentees'), mapping = aes(x = 
   geom_line() +
   scale_color_brewer(palette = 'Greens') +
   xlab('Day') +
-  theme_minimal()
+  theme_minimal() # Model appears to behave as expected
 
 remove(plot)
 
-# Visualize number sick workers at home and at work by agent type, dependent on maximum number of sick leave days
+# Visualize number of sick workers at home and at work by agent type, dependent on maximum number of sick leave days
 plot <- data %>%
   filter(number_of_workers == 200 & number_of_teams == 5 & movement_across_teams == 0.3 & share_of_presentees == 0.51) %>% # Calibrated values
   filter(run_number %in% seq(from = 1, to = 48600, by = 20)) %>% # Do not average out waves
@@ -74,7 +74,7 @@ ggplot(data = filter(plot, day <= 65 & group == 'Absentees'), mapping = aes(x = 
   geom_line() +
   scale_color_brewer(palette = 'Greens') +
   xlab('Day') +
-  theme_minimal()
+  theme_minimal() # Again, model appears to behave as expected
 
 remove(plot)
 
@@ -92,19 +92,53 @@ plot <- mean_productivity %>%
   gather(Workers, Absentees, Presentees, key = 'group', value = 'Mean Productivity')
 
 mean_productivity_plot_together <- ggplot(data = filter(plot, group == 'Workers'), mapping = aes(x = max_sick_leave_days, y = `Mean Productivity`)) +
-  geom_point(position = position_jitter(), size = 0.1) +
-  ylim(min(plot[, 'Mean Productivity']) - 0.01, max(plot[, 'Mean Productivity']) + 0.01) +
-  labs(x = 'Maximum Number of Sick Leave Days') +
-  theme_minimal()
+  geom_point(alpha = 1 / 100, position = position_jitter(), size = 1 / 10000) +
+  ylim(min(plot[, 'Mean Productivity']) - 1 / 100, max(plot[, 'Mean Productivity']) + 1 / 100) +
+  labs(title = str_wrap('Figure 1: Maximum Number of Sick Leave Days and Productivity, All Workers'), x = 'Maximum Number of Sick Leave Days') +
+  theme_minimal(base_size = 3) +
+  theme(plot.background = element_rect(color = 'black'))
 mean_productivity_plot_together
 
 mean_productivity_plot_separate <- ggplot(data = filter(plot, group %in% c('Absentees', 'Presentees')), mapping = aes(x = max_sick_leave_days, y = `Mean Productivity`, color = group)) +
-  geom_point(alpha = 0.5, position = position_jitter(), size = 0.1) +
+  geom_point(alpha = 1 / 100, position = position_jitter(), size = 1 / 10000) +
   scale_color_manual(values = c('Absentees' = 'grey', 'Presentees' = 'red')) +
-  ylim(min(plot[, 'Mean Productivity']) - 0.01, max(plot[, 'Mean Productivity']) + 0.01) +
-  labs(x = 'Maximum Number of Sick Leave Days', color = NULL) +
-  theme_minimal()
+  ylim(min(plot[, 'Mean Productivity']) - 1 / 100, max(plot[, 'Mean Productivity']) + 1 / 100) +
+  labs(title = str_wrap('Figure 2: Maximum Number of Sick Leave Days and Productivity, Absentees (Grey) and Presentees (Red)'), x = 'Maximum Number of Sick Leave Days') +
+  guides(color = 'none') +
+  theme_minimal(base_size = 3) +
+  theme(plot.background = element_rect(color = 'black'))
 mean_productivity_plot_separate
+
+remove(plot)
+
+# Extract average number of infections by agent type for whole year
+mean_infections <- data %>%
+  filter(day == 260) %>%
+  select(run_number, mean_infections_of_turtles, mean_infections_of_absentees, mean_infections_of_presentees, max_sick_leave_days, number_of_workers, number_of_teams, movement_across_teams, share_of_presentees) %>%
+  rename(workers = mean_infections_of_turtles, absentees = mean_infections_of_absentees, presentees = mean_infections_of_presentees)
+
+# Plot average number of infections by agent type for whole year, dependent on maximum number of sick leave days
+plot <- mean_infections %>%
+  rename(Workers = workers, Absentees = absentees, Presentees = presentees) %>%
+  gather(Workers, Absentees, Presentees, key = 'group', value = 'Average Number of Infections')
+
+mean_infections_plot_together <- ggplot(data = filter(plot, group == 'Workers'), mapping = aes(x = max_sick_leave_days, y = `Average Number of Infections`)) +
+  geom_point(alpha = 1 / 100, position = position_jitter(), size = 1 / 10000) +
+  ylim(min(plot[, 'Average Number of Infections']) - 1 / 100, max(plot[, 'Average Number of Infections']) + 1 / 100) +
+  labs(title = str_wrap('Figure 3: Maximum Number of Sick Leave Days and Average Number of Infections, All Workers'), x = 'Maximum Number of Sick Leave Days') +
+  theme_minimal(base_size = 3) +
+  theme(plot.background = element_rect(color = 'black'))
+mean_infections_plot_together
+
+mean_infections_plot_separate <- ggplot(data = filter(plot, group %in% c('Absentees', 'Presentees')), mapping = aes(x = max_sick_leave_days, y = `Average Number of Infections`, color = group)) +
+  geom_point(alpha = 1 / 100, position = position_jitter(), size = 1 / 10000) +
+  scale_color_manual(values = c('Absentees' = 'grey', 'Presentees' = 'red')) +
+  ylim(min(plot[, 'Average Number of Infections']) - 1 / 100, max(plot[, 'Average Number of Infections']) + 1 / 100) +
+  labs(title = str_wrap('Figure 4: Maximum Number of Sick Leave Days and Average Number of Infections, Absentees (Grey) and Presentees (Red)'), x = 'Maximum Number of Sick Leave Days') +
+  guides(color = 'none') +
+  theme_minimal(base_size = 3) +
+  theme(plot.background = element_rect(color = 'black'))
+mean_infections_plot_separate
 
 remove(plot)
 
@@ -122,47 +156,22 @@ plot <- sick_days %>%
   gather(Workers, Absentees, Presentees, key = 'group', value = 'Number of Sick Days (in %)')
 
 sick_days_plot_together <- ggplot(data = filter(plot, group == 'Workers'), mapping = aes(x = max_sick_leave_days, y = `Number of Sick Days (in %)`)) +
-  geom_point(position = position_jitter(), size = 0.1) +
-  ylim(min(plot[, 'Number of Sick Days (in %)']) - 0.01, max(plot[, 'Number of Sick Days (in %)']) + 0.01) +
-  labs(x = 'Maximum Number of Sick Leave Days') +
-  theme_minimal()
+  geom_point(alpha = 1 / 100, position = position_jitter(), size = 1 / 10000) +
+  ylim(min(plot[, 'Number of Sick Days (in %)']) - 1 / 100, max(plot[, 'Number of Sick Days (in %)']) + 1 / 100) +
+  labs(title = str_wrap('Figure 5: Maximum Number of Sick Leave Days and Number of Sick Days (in %), All Workers'), x = 'Maximum Number of Sick Leave Days') +
+  theme_minimal(base_size = 3) +
+  theme(plot.background = element_rect(color = 'black'))
 sick_days_plot_together
 
 sick_days_plot_separate <- ggplot(data = filter(plot, group %in% c('Absentees', 'Presentees')), mapping = aes(x = max_sick_leave_days, y = `Number of Sick Days (in %)`, color = group)) +
-  geom_point(alpha = 0.5, position = position_jitter(), size = 0.1) +
+  geom_point(alpha = 1 / 100, position = position_jitter(), size = 1 / 10000) +
   scale_color_manual(values = c('Absentees' = 'grey', 'Presentees' = 'red')) +
-  ylim(min(plot[, 'Number of Sick Days (in %)']) - 0.01, max(plot[, 'Number of Sick Days (in %)']) + 0.01) +
-  labs(x = 'Maximum Number of Sick Leave Days', color = NULL) +
-  theme_minimal()
+  ylim(min(plot[, 'Number of Sick Days (in %)']) - 1 / 100, max(plot[, 'Number of Sick Days (in %)']) + 1 / 100) +
+  labs(title = str_wrap('Figure 6: Maximum Number of Sick Leave Days and Number of Sick Days (in %), Absentees (Grey) and Presentees (Red)'), x = 'Maximum Number of Sick Leave Days') +
+  guides(color = 'none') +
+  theme_minimal(base_size = 3) +
+  theme(plot.background = element_rect(color = 'black'))
 sick_days_plot_separate
-
-remove(plot)
-
-# Extract average number of infections by agent type for whole year
-mean_infections <- data %>%
-  filter(day == 260) %>%
-  select(run_number, mean_infections_of_turtles, mean_infections_of_absentees, mean_infections_of_presentees, max_sick_leave_days, number_of_workers, number_of_teams, movement_across_teams, share_of_presentees) %>%
-  rename(workers = mean_infections_of_turtles, absentees = mean_infections_of_absentees, presentees = mean_infections_of_presentees)
-
-# Plot average number of infections by agent type for whole year, dependent on maximum number of sick leave days
-plot <- mean_infections %>%
-  rename(Workers = workers, Absentees = absentees, Presentees = presentees) %>%
-  gather(Workers, Absentees, Presentees, key = 'group', value = 'Average Number of Infections')
-
-mean_infections_plot_together <- ggplot(data = filter(plot, group == 'Workers'), mapping = aes(x = max_sick_leave_days, y = `Average Number of Infections`)) +
-  geom_point(position = position_jitter(), size = 0.1) +
-  ylim(min(plot[, 'Average Number of Infections']) - 0.01, max(plot[, 'Average Number of Infections']) + 0.01) +
-  labs(x = 'Maximum Number of Sick Leave Days') +
-  theme_minimal()
-mean_infections_plot_together
-
-mean_infections_plot_separate <- ggplot(data = filter(plot, group %in% c('Absentees', 'Presentees')), mapping = aes(x = max_sick_leave_days, y = `Average Number of Infections`, color = group)) +
-  geom_point(alpha = 0.5, position = position_jitter(), size = 0.1) +
-  scale_color_manual(values = c('Absentees' = 'grey', 'Presentees' = 'red')) +
-  ylim(min(plot[, 'Average Number of Infections']) - 0.01, max(plot[, 'Average Number of Infections']) + 0.01) +
-  labs(x = 'Maximum Number of Sick Leave Days', color = NULL) +
-  theme_minimal()
-mean_infections_plot_separate
 
 remove(plot)
 
@@ -170,67 +179,82 @@ remove(plot)
 mean_productivity_lm <- mean_productivity %>%
   gather(workers, absentees, presentees, key = 'group', value = mean_productivity) %>%
   mutate(group = as.factor(group)) %>%
-  mutate(group = relevel(group, 'workers'))
+  mutate(group = relevel(group, 'workers')) %>%
+  filter(max_sick_leave_days <= 20) # Constant for MAX-SICK-LEAVE-DAYS > 20
 
 lm(mean_productivity ~ group * max_sick_leave_days, mean_productivity_lm) %>%
   summary()
 
+mean_productivity_lm <- mutate(mean_productivity_lm, group = str_to_title(group))
+
 mean_productivity_plot_together +
-  geom_smooth(method = 'lm', color = 'black')
+  geom_smooth(data = filter(mean_productivity_lm, group == 'Workers'), mapping = aes(x = max_sick_leave_days, y = mean_productivity), method = 'lm', color = 'black', linewidth = 1 / 3, se = FALSE)
+ggsave('Figure 1.png', scale = 1 / 3)
 
 mean_productivity_plot_separate +
-  geom_smooth(method = 'lm')
+  geom_smooth(data = filter(mean_productivity_lm, group %in% c('Absentees', 'Presentees')), mapping = aes(x = max_sick_leave_days, y = mean_productivity, color = group), method = 'lm', linewidth = 1 / 3, se = FALSE)
+ggsave('Figure 2.png', scale = 1 / 3)
 
 remove(mean_productivity, mean_productivity_plot_separate, mean_productivity_plot_together)
-
-# Simple linear regression model for number of sick days in percent by agent type for whole year
-sick_days_lm <- sick_days %>%
-  gather(workers, absentees, presentees, key = 'group', value = sick_days) %>%
-  mutate(group = as.factor(group)) %>%
-  mutate(group = relevel(group, 'workers'))
-
-lm(sick_days ~ group * max_sick_leave_days, sick_days_lm) %>%
-  summary()
-
-sick_days_plot_together +
-  geom_smooth(method = 'lm', color = 'black')
-
-sick_days_plot_separate +
-  geom_smooth(method = 'lm')
-
-remove(sick_days, sick_days_plot_separate, sick_days_plot_together)
 
 # Simple linear regression model for average number of infections by agent type for whole year
 mean_infections_lm <- mean_infections %>%
   gather(workers, absentees, presentees, key = 'group', value = mean_infections) %>%
   mutate(group = as.factor(group)) %>%
-  mutate(group = relevel(group, 'workers'))
+  mutate(group = relevel(group, 'workers')) %>%
+  filter(max_sick_leave_days <= 20) # Constant for MAX-SICK-LEAVE-DAYS > 20
 
 lm(mean_infections ~ group * max_sick_leave_days, mean_infections_lm) %>%
   summary()
 
+mean_infections_lm <- mutate(mean_infections_lm, group = str_to_title(group))
+
 mean_infections_plot_together +
-  geom_smooth(method = 'lm', color = 'black')
+  geom_smooth(data = filter(mean_infections_lm, group == 'Workers'), mapping = aes(x = max_sick_leave_days, y = mean_infections), method = 'lm', color = 'black', linewidth = 1 / 3, se = FALSE)
+ggsave('Figure 3.png', scale = 1 / 3)
 
 mean_infections_plot_separate +
-  geom_smooth(method = 'lm')
+  geom_smooth(data = filter(mean_infections_lm, group %in% c('Absentees', 'Presentees')), mapping = aes(x = max_sick_leave_days, y = mean_infections, color = group), method = 'lm', linewidth = 1 / 3, se = FALSE)
+ggsave('Figure 4.png', scale = 1 / 3)
 
 remove(mean_infections, mean_infections_plot_separate, mean_infections_plot_together)
 
-# Multiple linear regression model for mean productivity by agent type for whole year
-lm(mean_productivity ~ group * max_sick_leave_days * (number_of_workers + number_of_teams + movement_across_teams + share_of_presentees), mean_productivity_lm) %>%
+# Simple linear regression model for number of sick days in percent by agent type for whole year
+sick_days_lm <- sick_days %>%
+  gather(workers, absentees, presentees, key = 'group', value = sick_days) %>%
+  mutate(group = as.factor(group)) %>%
+  mutate(group = relevel(group, 'workers')) %>%
+  filter(max_sick_leave_days <= 20) # Constant for MAX-SICK-LEAVE-DAYS > 20
+
+lm(sick_days ~ group * max_sick_leave_days, sick_days_lm) %>%
+  summary()
+
+sick_days_lm <- mutate(sick_days_lm, group = str_to_title(group))
+
+sick_days_plot_together +
+  geom_smooth(data = filter(sick_days_lm, group == 'Workers'), mapping = aes(x = max_sick_leave_days, y = sick_days), method = 'lm', color = 'black', linewidth = 1 / 3, se = FALSE)
+ggsave('Figure 5.png', scale = 1 / 3)
+
+sick_days_plot_separate +
+  geom_smooth(data = filter(sick_days_lm, group %in% c('Absentees', 'Presentees')), mapping = aes(x = max_sick_leave_days, y = sick_days, color = group), method = 'lm', linewidth = 1 / 3, se = FALSE)
+ggsave('Figure 6.png', scale = 1 / 3)
+
+remove(sick_days, sick_days_plot_separate, sick_days_plot_together)
+
+# Multiple linear regression model for mean productivity for whole year
+lm(mean_productivity ~ max_sick_leave_days * (number_of_workers + number_of_teams + movement_across_teams + share_of_presentees), filter(mean_productivity_lm, group == 'Workers')) %>%
   summary()
 
 remove(mean_productivity_lm)
 
-# Multiple linear regression model for number of sick days in percent by agent type for whole year
-lm(sick_days ~ group * max_sick_leave_days * (number_of_workers + number_of_teams + movement_across_teams + share_of_presentees), sick_days_lm) %>%
-  summary()
-
-remove(sick_days_lm)
-
-# Multiple linear regression model for average number of infections by agent type for whole year
-lm(mean_infections ~ group * max_sick_leave_days * (number_of_workers + number_of_teams + movement_across_teams + share_of_presentees), mean_infections_lm) %>%
+# Multiple linear regression model for average number of infections for whole year
+lm(mean_infections ~ max_sick_leave_days * (number_of_workers + number_of_teams + movement_across_teams + share_of_presentees), filter(mean_infections_lm, group == 'Workers')) %>%
   summary()
 
 remove(mean_infections_lm)
+
+# Multiple linear regression model for number of sick days in percent by agent type for whole year
+lm(sick_days ~ max_sick_leave_days * (number_of_workers + number_of_teams + movement_across_teams + share_of_presentees), filter(sick_days_lm, group == 'Workers')) %>%
+  summary()
+
+remove(sick_days_lm)
